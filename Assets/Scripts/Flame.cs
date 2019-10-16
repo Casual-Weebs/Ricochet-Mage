1f0f20497d53a688539bd27363eb740bdc09a77c
Destrile <47186911+Destrile@users.noreply.github.com>
2019-10-16 06:35:24 -0400

    Update
    Added Level Selection and started slingshot

---
+++ Assets/Scripts/Flame.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flame : MonoBehaviour
+{
+    private bool isPressed;
+
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update ()
+    {
+        if(isPressed)
+        {
+            DragBall();
+        }
+    }
+
+    private void DragBall()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        rb.position = mousePosition;
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+    }
+}
