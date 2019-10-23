53bd167b05f41f44084b0f4ebd8dec0ecfad65e1
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-10-23 19:31:45 -0400

    Class End Push

---
+++ Assets/GameWin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWin : MonoBehaviour
+{
+    public GameObject ricochetWin;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+        {
+                ricochetWin.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/Shot_Move.cs
+++ Assets/Scripts/Shot_Move.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Shot_Move : MonoBehaviour
 {
     public float speed;
+    public float lifetime;
     public Rigidbody2D rb;
+    
     void Start()
     {
         rb.velocity = transform.right * speed;
+        Destroy(gameObject, lifetime);
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
