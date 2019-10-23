453fe9239c366ae23fb77921f1332726f8dde29f
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-10-23 18:28:12 -0400

    Gamepleigh!!

---
+++ Assets/Scripts/Shot_Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot_Move : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/WandShot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandShot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
