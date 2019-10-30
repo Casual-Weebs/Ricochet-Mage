567e3af34460ac6c4ec0d8c68ffd1ffc9308dc4b
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-10-30 02:32:16 -0400

    Working Game Win/Loss with Mana

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ricochetWin;
+    public GameObject ricochetLose;
+
+    public float mana;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && mana > 0)
+        {
+            Shoot();
+        }
+        //Win
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+        {
+            ricochetWin.SetActive(true);
+        }
+        //Lose
+        if (GameObject.FindGameObjectsWithTag("Enemy").Length > 0 && GameObject.FindGameObjectsWithTag("Bullet").Length <= 0 && mana <= 0)
+        {
+            ricochetLose.SetActive(true);
+        }
+    }
+    void Shoot()
+    {
+        //shooting logic
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        mana--;
+    }
+}
--- Assets/Scripts/WandShot.cs
+++ Assets/Scripts/WandShot.cs
@@ -6,6 +6,7 @@ public class WandShot : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+   
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
