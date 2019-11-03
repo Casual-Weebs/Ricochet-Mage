cb5cf24c4207acf766bf2893886e70e3ab855d6b
fletchert986 <42794464+fletchert986@users.noreply.github.com>
2019-11-02 21:38:43 -0400

    Bullet Rotation

---
+++ Assets/Scripts/BulletRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletRotator : MonoBehaviour
+{
+    public float angularSpeed;
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, angularSpeed) * Time.deltaTime);
+    }
+}
